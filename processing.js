0f642f5ff981155a5bc4884a743c9d095a5207e9
MichaelJonathanLin <michael.jlin7@gmail.com>
2018-05-08 18:05:14 -0400

    Adding form submit functionality

---
+++ processing.js
@@ -0,0 +1,19 @@
+function getFormData(){
+
+  document.getElementById("sub").innerHTML = "Submitting";
+
+  //var delay = 2000;
+  //setTimeout(function(){},delay);
+  //  document.getElementById("sub").innerHTML = "Sign up now";
+
+var email = document.getElementById("inputEmail");
+var interest = document.getElementById("inputInterest");
+//document.getElementById("form-success").innerHTML ="";
+//document.getElementById("thanks").innerHTML ="Thanks for subscribing";
+//document.getElementById("thanksmsg").innerHTML ="You'll start receiving free tips and resources soon.";
+console.log (email);
+console.log (interest);
+
+  document.getElementById("sub").innerHTML = "Submitting";
+
+}
